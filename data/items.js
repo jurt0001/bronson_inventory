3c102b42f7e190c0505392853c1a2736b2ea6535
christianjurt <Longbeach@2>
2017-10-08 21:59:47 -0400

    moved some files around

---
+++ data/items.js
@@ -0,0 +1,31 @@
+/*****************************************************************
+File: Main.js
+Author: Christian Josef Jurt
+
+Description: This is React Application displays a list on the Main page.
+it also allows you to add a new item to the list.
+
+Version: 0.0.1
+Updated: Oct 1, 2017
+
+*****************************************************************/
+
+
+let items = [
+	{key: 1, id: 1, name: 'Beer', year: '700ml', description: "", image: 'images/beer.png'},
+	{key: 2, id: 2, name: 'White Wine', year: '2017', description:  "", image: 'images/white_wine.png'},
+	{key: 3, id: 3, name: 'Red Wine', year: '2017', description: "", image: 'images/red_wine.png'},
+	{key: 4, id: 4, name: 'Jameson', year: '2017', description: "", image: 'images/jameson.png'},
+    {key: 5, id: 5, name: 'Jack Daniels', year: '2017', description: "", image: 'images/jack.png'},
+    {key: 6, id: 6, name: 'Rum', year: '2017', description: "", image: 'images/bacardi.png'},
+    {key: 7, id: 7, name: 'Vodka', year: '2017', description: "", image: 'images/vodka.png'},
+    {key: 8, id: 8, name: 'Water', year: '350ml', description: "", image: 'images/water.png'},
+    {key: 9, id: 9, name: 'Cola', year: '2017', description: "", image: 'images/coke.png'},
+    {key: 10, id: 10, name: 'Ginger Ale', year: '2017', description: "", image: 'images/ginger_ale_can.png'},
+    {key: 11, id: 11, name: 'Diet Coke', year: '2017', description: "", image: 'images/diet_coke_can.png'},
+    {key: 12, id: 12, name: '7UP', year: '2017', description: "", image: 'images/7up_can.png'},
+    {key: 13, id: 13, name: 'Club Soda', year: '2017', description: "", image: 'images/club_soda_can.png'},
+    {key: 14, id: 14, name: 'Tonic', year: '2017', description: "", image: 'images/tonic_can.png'}
+    
+    ]
+    
